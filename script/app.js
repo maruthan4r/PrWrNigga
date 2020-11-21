fbe42a3bc749de24b8eeebc1b460fd069dcff42c
DragonCube <74070442+DragonCube1@users.noreply.github.com>
2020-11-21 20:07:31 +0100

    comments

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+//Get verschiedene HTML-Elemente
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
@@ -6,10 +7,14 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 const resultBoardElement = document.getElementById('result');
 const againButtonElement = document.getElementById('again-btn');
 const homeButtonElement = document.getElementById('home-btn');
+
+//Counter, der die richtigen Antworten zählt
 let correctQuestionCounter = 0;
 
+//shuffledQuestions wird eine zufällige Frage sein, currentQuestionIndex ist der curIndex vom questions Array
 let shuffledQuestions, currentQuestionIndex;
 
+//Verschiedene EventListener für die Buttons
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
@@ -18,10 +23,14 @@ nextButton.addEventListener('click', () => {
 againButtonElement.addEventListener('click', resetGame);
 homeButtonElement.addEventListener('click', home)
 
+//startGame startet das Spiel und wird durch den "Start" Button ausgelöst
 function startGame(){
+
+    //Bei Spielstart wird der Counter auf 0 gesetzt
     correctQuestionCounter = 0;
-    console.log('Started');
     startButton.classList.add('hide');
+
+    //Gibt eine zufällige Frage zurück und speichert sie in shuffledQuestions
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
@@ -29,14 +38,31 @@ function startGame(){
     setNextQuestion()
 }
 
+//Bereitet alles für das Anzeigen einer Frage vor
 function setNextQuestion(){
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
+//Entfernt alle Items im Container
+function resetState(){
 
+    //Versteckt den "Next" Button
+    nextButton.classList.add('hide');
+
+    //Entfernt Buttons aus dem Container, solange welche existieren
+    while(answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+//Zeigt die Frage an der Website an
 function showQuestion(question){
+
+    //Ersetzt den default Text durch die Frage
     questionElement.innerText = question.question;
+
+    //Erstellt so viele Buttons wie es Antworten gibt
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
@@ -44,25 +70,29 @@ function showQuestion(question){
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
+
+        //Erstellt für jeden Button einen EventListener falls er angeklickt wird
         button.addEventListener('click', selectAnswer);
+
+        //Fügt jeden Button in den Container hinzu
         answerButtonsElement.appendChild(button);
     })
 }
 
-function resetState(){
-    nextButton.classList.add('hide');
-    while(answerButtonsElement.firstChild){
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
+//Reagiert auf den EventListener eines Antwort-Buttons, Parameter "e" ist der angeklickte Button
 function selectAnswer(e){
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct;
+
+    /*
+    Erstellt ein Array, da die Buttons vorher nicht in einem Array gespeichert werden 
+    und führt für jeden Button die Funktion setStatusClass aus
+    */
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
     
+    //Kontrolliert ob noch Fragen übrig sind, falls nicht zeigt es das Resultat-Board an
     if(shuffledQuestions.length > currentQuestionIndex+1){
         nextButton.classList.remove('hide');
     } else {
@@ -70,13 +100,17 @@ function selectAnswer(e){
         resultBoardElement.classList.remove('hide');
     }
 
+    //Falls die Antwort richtig war wird der Counter erhöht
     if(selectedButton.dataset = correct){
         correctQuestionCounter++;
     }
+
+    //Ersetzt den default Wert des Resultat-Boards durch Anzahl Fragen und Anzahl richtiger Fragen
     document.getElementById('total-question').innerHTML = questions.length;
     document.getElementById('total-correct').innerHTML = correctQuestionCounter;
 }
 
+//Setzt für den Button den jeweiligen Status (Richtig oder Falsch)
 function setStatusClass(element, correct){
     clearStatusClass(element);
     if(correct){
@@ -86,20 +120,24 @@ function setStatusClass(element, correct){
     }
 }
 
+//Entfernt für den Button den jeweiligen Status (Richtig oder Falsch)
 function clearStatusClass(element){
     element.classList.remove('correct');
     element.classList.remove('wrong');
 }
 
+//Wird durch den EventListener des Buttons "Try Again" ausgeführt und resettet das Quiz
 function resetGame(){
     startButton.classList.remove('hide');
     resultBoardElement.classList.add('hide');
 }
 
+//Wird durch den Eventlistener des Buttons "Go To Home" ausgeführt und führt den User zum File "index.html"
 function home(){
     location.href = "index.html";
 }
 
+//Array von Fragen mit den jeweiligen Antworten
 const questions = [
     {
         question: "Ist Patrick fett?",
