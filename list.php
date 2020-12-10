b0547ce4af1006760ace2522ccfeb5c952c47262
maruthan4r <71565617+maruthan4r@users.noreply.github.com>
2020-12-10 18:13:28 +0100

    list.php
    Mit e liste vo de wichtigste Pc-Modell ide ziit. Isch nur en entwurf

---
+++ list.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <link rel="stylesheet" type="text/css" href="style/transition.css">
+    <link rel="stylesheet" href="style/stylenav.css">
+    <meta charset="utf-8">
+    <title>Computer</title>
+    <script defer src="script/transition.js"></script>
+    <script defer src="script/navbar.js"></script>
+    <script defer src="script/timeline.js"></script>
+</head>
+<body>
+<header>
+        <nav>
+            <div class="logo">
+                <h4><img src="img/24S2.gif" alt="PC" width="50" height="50"></h4>
+            </div>
+            <div>
+                <h4 class="logo">Geschichte des modernen Computers</h4>
+            </div>
+            <ul class="links">
+                <li>
+                    <a href="quiz.html" class="button transitionbutton">Quiz</a>
+                </li>
+                <li>
+                    <a href="form.php" class="button transitionbutton">Kontakt</a>
+                </li>
+                <li>
+                    <a class="button" onclick="darkmode()">DARKMODE</a>
+                </li>
+            </ul>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+    </header>
+
+<div class="listBox">
+
+<?php
+$servername = "localhost";
+$username = "pchistory";
+$password = "Thanabal44";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,'computers_main');
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+</div>
+
+
+
+    <script>
+        function darkmode() {
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+            document.getElementsByclassName("text").classList.toggle("dark-mode");
+        }
+    </script>
+</body>
+</html>
